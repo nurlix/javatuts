2211014d3e36d16ea4696069aa62654f0be37164
nurlix <regalix@gmail.com>
2020-03-05 17:28:09 +0600

    Create Predmet.java

---
+++ tut1/kgnu/kurs/Predmet.java
@@ -0,0 +1,14 @@
+package kgnu.kurs;
+
+public class Predmet {
+    //Matematika
+    public String predmet_naim;
+    public int[] reiting;
+
+    public Predmet() {}
+
+    public Predmet(String predmet_naim, int[] reiting) {
+        this.predmet_naim = predmet_naim;
+        this.reiting = reiting;
+    }
+}
