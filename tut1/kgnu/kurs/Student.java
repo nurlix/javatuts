1765dbd5803c41da96bc66fb25c9d280ba856b30
nurlix <regalix@gmail.com>
2020-03-05 17:28:35 +0600

    Update Student.java

--- tut1/kgnu/kurs/Student.java
+++ tut1/kgnu/kurs/Student.java
@@ -1,9 +1,13 @@
 package kgnu.kurs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student
 {
     public String FIO;
     public int Kurs;
+    public List<Predmet> predmets = new ArrayList();
 
     public Student() { }
 
